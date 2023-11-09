9f8079db060806d5f2b34b01df542352abb95365
yazanthvick <126107440+yazanthvick@users.noreply.github.com>
2023-11-08 23:00:59 -0500

    Delete Update bank project directory

--- Update bank project/src/BANKSYSTEM/CredentialUserTypes.java
+++
@@ -1,7 +0,0 @@
-package BANKSYSTEM;
-
-public interface CredentialUserTypes {
-    public static final int HEADOFFICE_USER = 1;
-    public static final int BRANCH_USER = 2;
-    public static final int CUSTOMER_USER = 3;
-}
